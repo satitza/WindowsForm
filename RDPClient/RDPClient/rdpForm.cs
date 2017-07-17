2040c2834fc569cdd97d883bec3b02007fb1ac1d
Satit Prontepanon <st_satitza@hotmail.com>
2017-07-17 19:30:40 +0700

    ImprementSendImageDesktop
    ImprementSendImageDesktop

--- RDPClient/RDPClient/rdpForm.cs
+++ RDPClient/RDPClient/rdpForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -25,6 +26,11 @@ namespace RDPClient
             Graphics graphics = Graphics.FromImage(screenshot);
             graphics.CopyFromScreen(bounds.X, bounds.X, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy);
             return screenshot;
+        }
+
+        {
+            mainStream = client.GetStream();
+            binFormatter.Serialize(mainStream, GrabDesktop());
         }
 
         public rdpForm()
