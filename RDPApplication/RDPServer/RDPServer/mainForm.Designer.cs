c7594f085c3b707a1bc0882d03ad0d5a77cbd92a
Satit Prontepanon <st_satitza@hotmail.com>
2017-07-17 20:20:33 +0700

    commit

---
+++ RDPApplication/RDPServer/RDPServer/mainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace RDPServer
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnListening = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Port";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(126, 19);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(209, 20);
+            this.txtPort.TabIndex = 1;
+            // 
+            // btnListening
+            // 
+            this.btnListening.Location = new System.Drawing.Point(126, 45);
+            this.btnListening.Name = "btnListening";
+            this.btnListening.Size = new System.Drawing.Size(209, 23);
+            this.btnListening.TabIndex = 2;
+            this.btnListening.Text = "Listening";
+            this.btnListening.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 86);
+            this.Controls.Add(this.btnListening);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label1);
+            this.Name = "mainForm";
+            this.Text = "Port Listening";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnListening;
+    }
+}
+
---
+++ RDPApplication/RDPServer/RDPServer/subForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDPServer
+{
+    public partial class subForm : Form
+    {
+        public int port;
+
+        public subForm(int Port)
+        {
+            if (Port == null)
+            {
+                throw new ArgumentNullException();
+            }
+            else if(Port > 0xffff)
+            {
+                throw new Exception("Port number is over 65535");
+            }
+            this.port = Port;
+            InitializeComponent();
+        }
+    }
+}
